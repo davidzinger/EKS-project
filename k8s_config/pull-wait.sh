6ec066f41ea56c0ec49f8e87d0fc303b425a6107
davidzinger <davidzingerman32@gmail.com>
2023-03-10 13:28:22 +0000

    Update pull-wait.sh

--- k8s_config/pull-wait.sh
+++ k8s_config/pull-wait.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# waits for image uploading to ECR, if it takes too long it will echo "failed ..." and will exit
 while ! aws ecr describe-images --repository-name $APP_NAME --image-ids imageTag=$ECR_TAG; do 
   echo "Waiting for Docker image to be available in ECR registry..."; 
   sleep 5; 
