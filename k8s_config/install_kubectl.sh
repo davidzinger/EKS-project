37dfbcd216675f4fe189622fd89540d41a735614
davidzinger <davidzingerman32@gmail.com>
2023-03-10 13:23:52 +0000

    Update install_kubectl.sh

--- k8s_config/install_kubectl.sh
+++ k8s_config/install_kubectl.sh
@@ -6,6 +6,5 @@ yum install -y curl
 # Download kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-# Install kubectl
 chmod +x kubectl
 mv kubectl /usr/local/bin/
