034bf616e735de319329f94aab65bcd654144539
davidzinger <davidzingerman32@gmail.com>
2023-03-10 13:03:43 +0000

    Add new file

---
+++ k8s_config/pull-wait.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while ! aws ecr describe-images --repository-name $APP_NAME --image-ids imageTag=$ECR_TAG; do 
+  echo "Waiting for Docker image to be available in ECR registry..."; 
+  sleep 5; 
+  counter=$((counter+1));
+  if [ $counter -eq 100 ]; then
+    echo "Failed to pull Docker image after 100 attempts. Exiting loop."
+    exit 1
+  fi
+done
